0db4af07028a0aab9a410bd02a040102436f514b
Flavian Kimani <githinjiflavian@gmail.com>
2024-10-29 08:03:21 +0300

    day 1

---
+++ src/main/java/com/learnerg/com/arrayshashing/ArraysHashingApplication.java
@@ -0,0 +1,12 @@
+package com.learnerg.com.arrayshashing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArraysHashingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArraysHashingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/learnerg/com/arrayshashing/ArraysHashingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.learnerg.com.arrayshashing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArraysHashingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
